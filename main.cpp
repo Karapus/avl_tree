be4904cf34e85faaf5462fe6fa08d7b2487c404e
Karapus <evgeny.nay@outlook.com>
2020-10-10 21:29:10 +0300

    add time mesurements

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
-#include <set>
 #include <queue>
 #include <utility>
 #include <iterator>
 #include <cassert>
+#include "AVL_set.hpp"
 
 #ifdef TIME
 #include <chrono>
 #endif
 
-namespace {
-	std::size_t range_query(const std::set<int> &set, std::pair<int, int> query) {
-		auto first_it = set.lower_bound(query.first);
-		auto second_it = set.upper_bound(query.second);
-		return std::distance(first_it, second_it);
-	}
-}
 
 int main() {
 	std::size_t n;
 	std::cin >> n;
-	std::set<int> set;
+	AVL::AVL_set_t<int> set;
 	for (auto i = 0LU; i < n; i++) {
 		int tmp = 0;
 		std::cin >> tmp;
@@ -35,18 +28,20 @@ int main() {
 		std::cin >> first >> second;
 		queries.emplace(first, second);
 	}
-
+	
 	std::queue<std::size_t> answers;
 #ifdef TIME
 	auto beg = std::chrono::high_resolution_clock::now();
 #endif
 	while (!queries.empty()) {
-		answers.emplace(range_query(set, queries.front()));
+		answers.emplace(set.range_query(queries.front()));
 		queries.pop();
 	}
 #ifdef TIME
 	auto end = std::chrono::high_resolution_clock::now();
 #endif
+	
+	
 	while (!answers.empty()) {
 		std::cout << answers.front() << ' ';
 		answers.pop();
